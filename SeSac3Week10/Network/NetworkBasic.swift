04346fbcdce650da7837d7e262f512eca89aea9b
yeomsungpil <present7256@gmail.com>
2023-09-19 15:15:08 +0900

    반복되는 코드 구조화 part4. 파라미터에 열거형 추가하기

--- SeSac3Week10/Network/NetworkBasic.swift
+++ SeSac3Week10/Network/NetworkBasic.swift
@@ -44,10 +44,7 @@ final class NetworkBasic {
     // 왜 옵셔널인 이유? - 디코딩 유무에 따라 옵셔널로 대응하면 하나는 nil이면 하나는 성공이기 때문
     // 컴플리션으로 여러가지의 매개변수를 받을때 성공, 실패 케이스만 얻으면 되는데, 다른 불필요한 경우의 수 발생
     // => Result 타입 : 성공 , 실패 케이스만 가져옴!         @escaping(Photo?, Error?)
-    func request(query: String, completion: @escaping(Result<Photo, Error>) -> Void) { // search Photo
-        
-        // 매개변수를 사용하기위해 열거형에 만들어둔 query 매개변수 사용 
-        let api = SesacAPI.search(query: query)
+    func request(api: SesacAPI, query: String, completionHandler: @escaping(Result<Photo, SeSacError>) -> Void) { // search Photo
         // queryString은 길이 제한이 있어서 간소한 정보만 받을 수 있음 -> parameters 사용
         // Post : 대량의 데이터를 서버에 추가하는 역할 <- 광범위한 범위 ex) 1000자 HTTPBody: Parameter
         // encoding : 파라미터를 queryString으로 보낼 수 있게 도와줌, default는 .httpBody로 되어 있음
@@ -56,44 +53,46 @@ final class NetworkBasic {
             .responseDecodable(of: Photo.self) { response in
                 switch response.result {
                 case .success(let data):
-                    completion(.success(data)) // 성공에 대한 데이터만 던짐
+                    completionHandler(.success(data)) // 성공에 대한 데이터만 던짐
                 case .failure(_):
                     // response.response?에서 만약에 nil이 나오면 500을 리턴 해준다.
                     let statusCode = response.response?.statusCode ?? 500
                     guard let error = SeSacError(rawValue: statusCode) else { return }
-                    completion(.failure(error)) // 실패에 대해서만 던짐
+                    completionHandler(.failure(error)) // 실패에 대해서만 던짐
                 }
             }
     }
     // @escaping (PhotoResult?, Error?)
-    func random(completionHandler: @escaping (Result<PhotoResult,Error>) -> Void) { // random Photo
+    func random(api: SesacAPI,completionHandler: @escaping (Result<PhotoResult, SeSacError>) -> Void) { // random Photo
         
-        let api = SesacAPI.random
-      
         // PhotoResult : 구조를 보면 Photo의 구조와 유사함... 하나의 구조체를 추가하지 않고 그저 활용하면 됨
         AF.request(api.endPoint, method: api.method, headers: api.header)
             .responseDecodable(of: PhotoResult.self) { response in
                 switch response.result {
                 case .success(let data):
                     completionHandler(.success(data))
-                case .failure(let error):
-                    completionHandler(.failure(error))
+                case .failure(_):
+                    // response.response?에서 만약에 nil이 나오면 500을 리턴 해준다.
+                    let statusCode = response.response?.statusCode ?? 500
+                    guard let error = SeSacError(rawValue: statusCode) else { return }
+                    completionHandler(.failure(error)) // 실패에 대해서만 던짐
                 }
             }
     }
 
     // 사진 한장에 대한 자세한 정보를 알고 싶다 detail
-    func detailPhoto(id: String, completionHandler: @escaping(Result<PhotoResult, Error>) -> Void) { // detail Photo
-        
-        let api = SesacAPI.detailPhoto(id: id)
+    func detailPhoto(api: SesacAPI, id: String, completionHandler: @escaping(Result<PhotoResult, SeSacError>) -> Void) { // detail Photo
         
         AF.request(api.endPoint, method: api.method, headers: api.header)
             .responseDecodable(of: PhotoResult.self) { response in
                 switch response.result {
                 case .success(let data):
                     completionHandler(.success(data))
-                case .failure(let error):
-                    completionHandler(.failure(error))
+                case .failure(_):
+                    // response.response?에서 만약에 nil이 나오면 500을 리턴 해준다.
+                    let statusCode = response.response?.statusCode ?? 500
+                    guard let error = SeSacError(rawValue: statusCode) else { return }
+                    completionHandler(.failure(error)) // 실패에 대해서만 던짐
                 }
             }
     }
