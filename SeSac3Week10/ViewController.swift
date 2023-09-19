853d8e5bf28363d940253b10115e866e427be6be
yeomsungpil <present7256@gmail.com>
2023-09-19 09:56:34 +0900

    Initial Commit

---
+++ SeSac3Week10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSac3Week10
+//
+//  Created by 염성필 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
