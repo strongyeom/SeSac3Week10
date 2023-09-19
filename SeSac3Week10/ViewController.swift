f3dd2a11c6e3ddc2eb6abccf8c33d7137285feda
yeomsungpil <present7256@gmail.com>
2023-09-19 10:23:58 +0900

    초기 설정 - 모델 생성 및 Alamofire를 통한 네트워크 통신

--- SeSac3Week10/ViewController.swift
+++ SeSac3Week10/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Alamofire
+
+// 네트워크 통신 -> 개선 -> 업데이트
 
 class ViewController: UIViewController {
 
@@ -13,7 +16,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func request() {
+        let key = "R87kkJUhEVTR_QPQo8pQOj7Q7sgWnUP8gTE8h0yOHB0"
+        let url = "https://api.unsplash.com/search/photos?query=cat&client_id=\(key)"
+        
+        AF.request(url, method: .get)
+            .responseDecodable(of: Photo.self) { response in
+                switch response.result {
+                case .success(let data):
+                    dump(data)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+      
+    }
 
 
 }
+
+// Codable : Decodable(외부에 있는 데이터를 우리가 사용할 수 있는 형식으로 변환 ) + Encodable(우리가 가지고 있는 데이터를 변환후 외부로 전달 )
+struct Photo: Decodable {
+    let total: Int
+    let total_pages: Int
+    let results: [PhotoResult]
+}
+
+struct PhotoResult: Decodable {
+    let id: String
+    let created_at: String
+    let urls: PhtoURL
+}
+
+struct PhtoURL :Decodable {
+    let full: String
+    let thumb: String
+}
+
 
